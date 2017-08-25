5d13af2caf224bfc85b8571b76ed52a91529d230
tmstani23 <tmstani23@gmail.com>
2017-08-25 11:33:16 -0500

    Unique array algorithm update
    Added an algo that compares two arrays and creates a new array
    containing the elements that differ between the two arrays.

--- int_algo_lib.js
+++ int_algo_lib.js
@@ -1,6 +1,6 @@
 
 //This function sums all numbers in a range and returns the total:
-function sumAll(arr) {
+function sumRange(arr) {
     var arr2 = [];
     var maxN = Math.max(...arr);
     var minN = Math.min(...arr);
@@ -23,7 +23,46 @@ function sumAll(arr) {
     //here the accumulator is sum, currentValue is same as index value
       //and return value is sum + currentValue
     //the second parameter 0 is the initial starting value
-    console.log(total);
+    //console.log(total);
     return total;
   }
-  sumAll([1, 4]);
+  //sumRange([1, 4]);
+
+
+  
+
+function diffArray(arr1, arr2) {
+  //Compare two arrays and return a new array 
+    //with any items only found in one of the two given arrays, 
+    //but not both. 
+  
+  var newArr = [];
+  var arrLength;
+  //set the arrLength variable to the largest of the two input arrays
+  if (arr2.length > arr1.length) {
+    arrLength = arr2.length;
+  }
+  else {
+    arrLength = arr1.length;
+  }
+  //loop i times based on the longest array:
+  for (i = 0; i < arrLength; i++) {
+    //check arr2 to verify if it doesn't contain current index element of arr1:
+    var checkArr2 = arr2.indexOf(arr1[i]) === -1;
+    //check arr2 to verify if it doesn't contain current index element of arr1:
+    var checkArr1 = arr1.indexOf(arr2[i]) === -1;
+    //if the element isnt in arr1:
+    if(checkArr2 && arr1[i] !== undefined){
+      //push the element to the new array
+      newArr.push(arr1[i]);
+    }
+    //if the element isnt in arr2:
+    if(checkArr1 && arr2[i] !== undefined){
+      //push the element to the new array
+      newArr.push(arr2[i]);
+    }
+  }
+  //console.log(newArr);
+  return newArr;
+}
+//diffArray([1, "calf", 3, "piglet"], [7, "filly"]);
